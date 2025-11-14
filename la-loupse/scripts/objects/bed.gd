4ac395dfdff8b82cf626c30a9f91a2ad2bbc88fb
Patrick <103536709+patrickycx@users.noreply.github.com>
2025-11-14 23:47:22 +0100

    Add interaction system for objects and player
    Introduces interactable and interacting components, enabling player-object interactions via the 'E' key. Adds scripts and scenes for interactable objects (bed, PC), updates player and office scenes to support interaction, and configures input and physics layers for interaction events.

---
+++ la-loupse/scripts/objects/bed.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+# INTERACTION
+
+# Import detectable component
+@onready var interactable: Area2D = $Interactable
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+# Detect the interaction (if E is pressed)
+func _ready() -> void:
+	interactable.interact = _on_interact
+
+# What will happen after pressing E
+func _on_interact():
+	# TODO
+	print("Success: I sleep.")
