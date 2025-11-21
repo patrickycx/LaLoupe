b641c352485ce75d067e6a01e9b76f938174c613
Patrick <103536709+patrickycx@users.noreply.github.com>
2025-11-21 21:51:06 +0100

    Small changes on the last day of Game jam

--- la-loupse/scripts/ui/coverChapter.gd
+++ la-loupse/scripts/ui/coverChapter.gd
@@ -1,6 +1,6 @@
 extends Control
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventJoypadButton or Input.is_action_just_pressed("continue"):
+	if Input.is_action_just_pressed("continue"):
 		print("Next Chapter")
 		get_tree().change_scene_to_file(Global.destination)
